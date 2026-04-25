efc5a276b0a4bcd06f5ad9dec32d28cdaa2c255a
Paulo Ventura <pe.martins.ventura@gmail.com>
2026-04-25 15:59:39 -0400

    tuff

--- src/helpers.py
+++ src/helpers.py
@@ -41,13 +41,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return old_nodes
 
 def extract_markdown_images(text):
-    
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
     
 
 def extract_markdown_links(text):
-    
-    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
 
 def split_nodes_image(old_nodes):
     new_nodes = []
@@ -58,56 +56,56 @@ def split_nodes_image(old_nodes):
             else:
                 markdowns = extract_markdown_images(node.text)
 
+                if not markdowns:
+                    new_nodes.append(node)
+                    continue
+
                 text = node.text
                 for markdown in markdowns:
                     pattern = f"![{markdown[0]}]({markdown[1]})"
-                
                     parts = text.split(pattern, 1)
-                    
-                    if len(parts) == 1:
 
-                        if parts[0] != "":
-                            new_nodes.append(TextNode(text=markdown[0], text_type=TextType.IMAGE_TEXT, url=markdown[1]))
-
-                        continue
-                    
-                    else:
+                    if parts[0] != "":
                         new_nodes.append(TextNode(text=parts[0], text_type=TextType.PLAIN_TEXT))
 
-                        new_nodes.append(TextNode(text=markdown[0], text_type=TextType.IMAGE_TEXT, url=markdown[1]))
+                    new_nodes.append(TextNode(text=markdown[0], text_type=TextType.IMAGE_TEXT, url=markdown[1]))
 
-                        text = parts[1]
+                    text = parts[1] if len(parts) > 1 else ""
+
+                if text != "":
+                    new_nodes.append(TextNode(text=text, text_type=TextType.PLAIN_TEXT))
+
         return new_nodes
     return old_nodes
 
 def split_nodes_link(old_nodes):
     new_nodes = []
-    if old_nodes:
-        for node in old_nodes:
-            if node.text_type != TextType.PLAIN_TEXT:
-                new_nodes.append(node)
-            else:
-                markdowns = extract_markdown_links(node.text)
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN_TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK_TEXT, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.PLAIN_TEXT))
+    return new_nodes
 
-                text = node.text
-
-                for markdown in markdowns:
-                    pattern = f"[{markdown[0]}({markdown[1]})]"
-                
-                    parts = text.split(pattern, 1)
-
-                    if len(parts) == 1:
-
-                        if parts[0] != "":
-                            new_nodes.append(TextNode(text=markdown[0], text_type=TextType.LINK_TEXT, url=markdown[1]))
-
-                        continue
-                    
-                    else:
-                        new_nodes.append(TextNode(text=parts[0], text_type=TextType.PLAIN_TEXT))
-
-                        new_nodes.append(TextNode(text=markdown[0], text_type=TextType.LINK_TEXT, url=markdown[1]))
-
-                        text = parts[1]
-        return new_nodes
-    return old_nodes
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.PLAIN_TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD_TEXT)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC_TEXT)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE_TEXT)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,7 +1,7 @@
 import unittest
 from textnode import TextNode, TextType
 
-from helpers import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from helpers import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestHelpers(unittest.TestCase):
     def test_text(self):
@@ -45,5 +45,24 @@ class TestHelpers(unittest.TestCase):
             ],
             new_nodes,
         )
+    
+    def test_text_to_textnode(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+        text_nodes = text_to_textnodes(text)
+
+        self.assertListEqual([
+            TextNode("This is ", TextType.PLAIN_TEXT),
+            TextNode("text", TextType.BOLD_TEXT),
+            TextNode(" with an ", TextType.PLAIN_TEXT),
+            TextNode("italic", TextType.ITALIC_TEXT),
+            TextNode(" word and a ", TextType.PLAIN_TEXT),
+            TextNode("code block", TextType.CODE_TEXT),
+            TextNode(" and an ", TextType.PLAIN_TEXT),
+            TextNode("obi wan image", TextType.IMAGE_TEXT, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.PLAIN_TEXT),     
+            TextNode("link", TextType.LINK_TEXT, "https://boot.dev"),        
+            ], text_nodes)      
+        
 if __name__ == "__main__":
     unittest.main()
