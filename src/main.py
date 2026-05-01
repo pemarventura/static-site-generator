635e64eeb14174cef840ab2286070782d45856c7
Paulo Ventura <pe.martins.ventura@gmail.com>
2026-05-01 18:54:08 -0400

    finished

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from textnode import TextNode, TextType
-
-def main():
-    text_node = TextNode("Hello World", TextType.PLAIN_TEXT, "a.url.com")
-
-    print(text_node)
-
-if __name__ == "__main__":
-    main()
